b52e4c45d7d151706fcc526e86459f75d9f07fa1
hene <hene@vaadin.com>
2011-08-31 10:08:36 +0300

    Added Apache 2.0 License information to Java files

--- src/org/vaadin/gwtgraphics/client/Shape.java
+++ src/org/vaadin/gwtgraphics/client/Shape.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Henri Kerola
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.vaadin.gwtgraphics.client;
 
 import org.vaadin.gwtgraphics.client.animation.Animatable;
@@ -6,11 +21,7 @@ import org.vaadin.gwtgraphics.client.animation.Animatable;
  * Shape is an abstract upper-class for VectorObjects that support filling,
  * stroking and positioning.
  * 
- * @author Henri Kerola / IT Mill Ltd
- * 
- */
-/**
- * @author henri
+ * @author Henri Kerola
  * 
  */
 public abstract class Shape extends VectorObject implements Strokeable,
--- src/org/vaadin/gwtgraphics/client/impl/util/SVGUtil.java
+++ src/org/vaadin/gwtgraphics/client/impl/util/SVGUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Henri Kerola
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.vaadin.gwtgraphics.client.impl.util;
 
 import com.google.gwt.dom.client.Element;
@@ -5,7 +20,7 @@ import com.google.gwt.dom.client.Element;
 /**
  * This class contains helpers used by the SVGImpl class.
  * 
- * @author Henri Kerola / IT Mill Ltd
+ * @author Henri Kerola
  * 
  */
 public abstract class SVGUtil {
